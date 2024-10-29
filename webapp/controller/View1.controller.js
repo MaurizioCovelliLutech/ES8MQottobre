99da8c8d9df6fa23ff7ab821d4cfdefca3176f5b
Maurizio Covelli <maurizio.covelli@lutech.it>
2024-10-29 16:35:07 +0000

    listreport senza filtri e navigazione

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,47 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel"
+], function (Controller, JSONModel, ODataModel) {
     "use strict";
 
+    const sURL = "/V2/Northwind/Northwind.svc/";
+ 
     return Controller.extend("project1.controller.View1", {
+
         onInit: function () {
 
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("RouteView1").attachPatternMatched(this.onRouteMatched, this);
+                
+        },
+
+        onRouteMatched: async function () {
+            var oModel = new ODataModel(sURL);
+
+            const oData = await new Promise((resolve, reject) => {
+                oModel.read("/Orders", {
+
+                    urlParameters: {
+                        "$expand": "Customer"
+                    },
+                success: function(oData, response) {
+                    resolve(oData);
+                },
+                    error: function(error) {
+                }
+        });
+    });         
+                this.getView().setModel(new JSONModel(oData.results), "modOrders");
+        },
+
+        onObjectPage: function(oEvent){
+
+            const newData = btoa(JSON.stringify(this.getView().getModel("modOrders").getData()));
+
+            var oRouter = this.getOwnerComponent().getRouter();
+              oRouter.navTo("RouteView2", {RouteView1:newData});
+              sap.m.MessageToast.show("CIAO BELLO");
         }
     });
 });
---
+++ webapp/controller/View2.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel"
+], function (Controller, JSONModel, ODataModel) {
+    "use strict";
+
+    const sURL = "/V2/Northwind/Northwind.svc/";
+ 
+    return Controller.extend("project1.controller.View1", {
+
+        onInit: function () {},
+
+        onRouteMatched: async function () {}
+           
+    });
+});
